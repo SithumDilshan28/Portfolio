d8d159edad6e9bbd7528d15608f6f87bf552e8de
Sithum Dilshan <shanukasithum28@gmail.com>
2023-05-19 22:22:51 +0530

    About Us Section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
-import { AppWrap, MotionWrap } from '../../wrapper';
-import './About.scss';
-import { urlFor, client } from '../../client';
+import { AppWrap, MotionWrap } from "../../wrapper";
+import "./About.scss";
+import { urlFor, client } from "../../client";
 
 const About = () => {
   const [abouts, setAbouts] = useState([]);
@@ -18,36 +18,48 @@ const About = () => {
 
   return (
     <>
-      <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
+      {/* About Us Section */}
+      <h2 className="head-text">
+        I Know that <span>Good Design</span> <br />
+        means <span>Good Business</span>
+      </h2>
 
       <div className="app__profiles">
         {abouts.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: 'tween' }}
+            transition={{ duration: 0.5, type: "tween" }}
             className="app__profile-item"
             key={about.title + index}
           >
             <img src={urlFor(about.imgUrl)} alt={about.title} />
-            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
-            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
-          </motion.div>  
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
         ))}
-        
       </div>
 
       <div class="CV">
-           <a  target="_blank" download="cv.pdf" href="cv.pdf" class="btn btn-download">Download CV</a>
+        <a
+          target="_blank"
+          download="cv.pdf"
+          href="cv.pdf"
+          class="btn btn-download"
+        >
+          Download CV
+        </a>
       </div>
-
     </>
   );
-
 };
 
 export default AppWrap(
-  MotionWrap(About, 'app__about'),
-  'about',
-  'app__whitebg',
+  MotionWrap(About, "app__about"),
+  "about",
+  "app__whitebg"
 );
